d4669eb297f203c0c07a255837217f7a090bc2e1
Leocone <wlia@163.com>
2017-02-17 09:50:03 +0800

    init

---
+++ csharp/appendix-a/HelloWorldProducer/hello_world_producer.cs
@@ -0,0 +1,61 @@
+/*##############################################
+# RabbitMQ in Action
+# Appendix A- Hello World Producer (.NET)
+# 
+# Requires: 
+#       * RabbitMQ.Client >= 2.7.0
+# 
+# Author: Jason J. W. Williams
+# (C)2011
+##############################################*/
+
+using System;
+using System.Text;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace HelloWorld {
+    class Producer {
+        
+        public static void Main(string[] args) {
+            
+            //#/(hwpdn.1) Collect server and message
+            if(args.Length < 2) {
+                Console.WriteLine("Must supply hostname and " +
+                                  "message text.");
+                Environment.Exit(-1);
+            }
+            
+            var conn_factory  = new ConnectionFactory();
+            
+            conn_factory.HostName = args[0];
+            conn_factory.UserName = "guest";
+            conn_factory.Password = "guest";
+            
+            //#/(hwpdn.2) Establish connection to broker
+            IConnection conn = conn_factory.CreateConnection();
+            IModel chan = conn.CreateModel();  //#/(hwpdn.2) Obtain channel
+            
+            //#/(hwpdn.3) Declare the exchange
+            chan.ExchangeDeclare("hello-exchange",
+                                 ExchangeType.Direct,
+                                 true,
+                                 false,
+                                 null);
+            
+            //#/(hwpdn.4) Create a plaintext message
+            string msg_body = args[1];
+            IBasicProperties msg_props = chan.CreateBasicProperties();
+            msg_props.ContentType = "text/plain";
+            
+            //#/(hwpdn.5) Publish the message
+            chan.BasicPublish("hello-exchange",
+                              "hola",
+                              msg_props,
+                              Encoding.ASCII.GetBytes(msg_body));
+            
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ erlang/chapter-12/rabbitmq-recent-history-exchange/package.mk
@@ -0,0 +1,3 @@
+DEPS:=rabbitmq-server rabbitmq-erlang-client
+RETAIN_ORIGINAL_VERSION:=true
+
